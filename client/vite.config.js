e7a87b845aa0d96239e43361c2861b9a6e16a51e
Anup <anuppoudel60@gmail.com>
2025-08-06 16:52:57 +1000

    vite config added

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+// client/vite.config.js
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  root: "./src", // Your entry point (where index.html is)
+  build: {
+    outDir: "../dist", // Output to client/dist
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "./src/index.html"), // Your entry HTML file
+    },
+  },
+  server: {
+    port: 5173, // Vite dev server port
+  },
+});
