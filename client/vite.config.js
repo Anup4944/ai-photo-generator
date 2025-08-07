b72f5546c5f3f29224dffbc0f06786dd5aa3a5fd
Anup <anuppoudel60@gmail.com>
2025-08-07 11:10:33 +1000

    changes to package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,17 @@
-// client/vite.config.js
-import { defineConfig } from "vite";
-import path from "path";
+// // client/vite.config.js
+// import { defineConfig } from "vite";
+// import path from "path";
 
-export default defineConfig({
-  root: "./src", // Your entry point (where index.html is)
-  build: {
-    outDir: "../dist", // Output to client/dist
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, "./src/index.html"), // Your entry HTML file
-    },
-  },
-  server: {
-    port: 5173, // Vite dev server port
-  },
-});
+// export default defineConfig({
+//   root: "./src", // Your entry point (where index.html is)
+//   build: {
+//     outDir: "../dist", // Output to client/dist
+//     emptyOutDir: true,
+//     rollupOptions: {
+//       input: path.resolve(__dirname, "./index.html"), // Your entry HTML file
+//     },
+//   },
+//   server: {
+//     port: 5173, // Vite dev server port
+//   },
+// });
